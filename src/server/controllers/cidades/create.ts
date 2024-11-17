bd6731c28306141f1020cf4316aaab885daf7e1d
Marcelo Siqueira Oliveira <mss.siqueira@gmail.com>
2024-11-17 17:02:14 -0300

    feat: validação de dados, versão 1

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,14 +1,35 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
+
 
 interface Icidade {
     nome: string;
 }
 
-export const create = (req: Request<{}, {}, Icidade>, res: Response) => {
-    console.log(req.body);
+const bodyValidation: yup.Schema<Icidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
 
+export const create = async(req: Request<{}, {}, Icidade>, res: Response) => {
+    let validatedData: Icidade | undefined= undefined;
+
+    try{
+        await bodyValidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.json({
+            errors: {
+                default: yupError.message,
+        }
+        })
+    }
+
+    console.log(validatedData);
 
 
     return res.send('create');
-};
+
+}
 
